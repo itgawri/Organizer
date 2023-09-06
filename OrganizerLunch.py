b9204c2d96fe44ced3f5260e96e21259b395cb0d
19gaw <19itgawri97@gmail.com>
2023-09-06 11:57:03 +0200

    First Project (EN/PL)

--- OrganizerLunch.py
+++ OrganizerLunch.py
@@ -12,3 +12,20 @@ enough_biryani = biryani_prepared == biryani_needed
 guests += 1
 biryani_per_guest = biryani_prepared // guests
 print(biryani_per_guest)
+
+# PL----------------------------------------------------------
+
+goscie = 19
+element_menu = "biryani"
+element_menu2 = "pizza"
+
+print("Stare menu to: " + element_menu)
+print("Nowe menu to: " + element_menu2)
+
+biryani_na_osobe = 1
+potrzebne_biryani = biryani_na_osobe * goscie
+przygotowane_biryani = 20
+wystarczajaco_biryani = przygotowane_biryani == potrzebne_biryani
+goscie += 1
+biryani_na_goscia = przygotowane_biryani // goscie
+print(biryani_na_goscia)
